4f1b5cc6766ab9aae5575eb3c8e1c7bbf87819b2
Vladimir Turomsha <zver010810@gmail.com>
2025-02-19 02:48:53 +0300

    make one populate query for ru_subscription

--- sql_scripts/populate_ru_subscritpion.sql
+++ sql_scripts/populate_ru_subscritpion.sql
@@ -5,10 +5,6 @@ SET ru_description =
         WHEN subscription_name = 'monthly_small' THEN '{"buy_info": "🏆Gold – выбор тех, кто хочет больше", "price_hint": "☕ Вместо трёх чашек кофе – контент, который собирает лайки!"}'::jsonb
         WHEN subscription_name = 'monthly_middle' THEN '{"buy_info": "💎 Platinum – продвинутый уровень", "price_hint": "🚕 Вместо одной поездки на такси – целый месяц крутых снимков!"}'::jsonb
         WHEN subscription_name = 'monthly_large' THEN '{"buy_info": "👑 Diamond", "price_hint": "💐 Вместо букета цветов – фотосессия, которая покоряет!"}'::jsonb
-        ELSE ru_description
-    END
-WHERE subscription_name IN ('monthly_bomz', 'monthly_small', 'monthly_middle', 'monthly_large');
-
-UPDATE public.subscriptions_details
-SET ru_description = '{}'::jsonb
-where subscription_type != 'monthly'
+        ELSE '{}'::jsonb
+    end
+WHERE subscription_name IS NOT NULL
